4a42896b2d8dd1ca09d83573b9dfbde976a86230
toskafke <denis.janiak7@gmail.com>
2020-10-26 21:14:32 +0100

    Add sentence segmentation and naive word tokenizer

--- NLA/tokenizer.py
+++ NLA/tokenizer.py
@@ -1,29 +1,34 @@
+import re
 from typing import List
 
 
 class Tokenizer:
-    def __init__(self, end_of_sentence_marks: List[str] = None):
-        if end_of_sentence_marks is not None:
-            self.end_of_sentence_marks = end_of_sentence_marks
-        else:
-            self.end_of_sentence_marks = ['.', '?', '!']
+    def sentencize(self, text: str) -> List[str]:
+        text = re.sub(r"\[\w\d*\]", '', text)
+        text = re.sub(r"([A-Z][a-z]*\sim\.)", r"\1<A>", text)
+        text = re.sub(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', '<S>', text)
+        text = text.replace('<A>', '')
+        text = text.split('<S>')
+        return text
 
-    def tokenize(self, text: str):
-        output = list()
-        text = [text]
-        for mark in self.end_of_sentence_marks:
-            output.extend([sentence.split(mark) for sentence in text])
-            text, ouput = output[0], []
+    def tokenize(self, text: str) -> List[str]:
+        token_pat = r"(?=\W)|(?<=\W)|\s+"
+        text = re.sub(token_pat, "<T>", text)
+        text = [x for x in text.split("<T>") if x]
+        text = ["<SOS>"] + text + ["<EOS>"]
         return text
 
 
 if __name__ == '__main__':
-    text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. " \
-           "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, " \
-           "when an unknown printer took a galley of type and scrambled it to make a type " \
-           "specimen book. It has survived not only five centuries, but also the leap into " \
-           "electronic typesetting, remaining essentially unchanged. It was popularised in " \
-           "the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, " \
-           "and more recently with desktop publishing software like Aldus PageMaker " \
-           "including versions of Lorem Ipsum."
-    print(Tokenizer().tokenize(text))
+    with open('lem2.txt', 'r', encoding='utf-8') as f:
+        text = f.read().replace('\n', ' ').replace('  ', ' ')
+
+    tokenizer = Tokenizer()
+    sentences = tokenizer.sentencize(text)
+
+    for i, sent in enumerate(sentences):
+        print(f"Sentence ({i}): {sent}")
+        tokens = tokenizer.tokenize(sent)
+        for i, t in enumerate(tokens):
+            print(f"Token ({i}): {t}")
+        print()
