1f7473aefcb33ac64dc4adede6c8b7bf9c6cca99
Piotr Kolebski <piotrkolebski@gmail.com>
2020-11-03 16:16:04 +0100

    Add KRNNT tagger

--- NLA/tagger.py
+++ NLA/tagger.py
@@ -20,6 +20,15 @@ class TaggerClass(abc.ABC):
         pass
 
 
+class KrnntTagger(TaggerClass):
+    def __init__(self):
+        self.url = "http://localhost:9003/?output_format=xces"
+
+    def tag(self, text):
+        results = requests.post(self.url, data=text.encode('utf-8'))
+        return results.text
+
+
 class ClarinTagger(TaggerClass):
     def __init__(self, url=URL, lpmn=LPMN, email_address="myemail@email.com"):
         self.url = url
@@ -57,6 +66,7 @@ class SpacyTagger(TaggerClass):
 @click.option('--file-path', '-fp',
               type=click.STRING,
               default="data/test-raw.txt",
+              # default="/home/pkolebski/Projects/semester2/nla/NLA/NLA/wiki_data/wiki_train_34_categories_data/Albania_59.txt",
               help="Path to the file you want to tag")
 @click.option('--lpmn', '-l',
               type=click.STRING,
@@ -66,9 +76,10 @@ class SpacyTagger(TaggerClass):
 def run_tagger(file_path, lpmn):
     with open(file_path, encoding='utf-8') as f:
         text = f.read()
-    tagger = ClarinTagger(lpmn=lpmn)
+    tagger = DockerTagger()
     tagged_text = tagger.tag(text)
-    with open("output_wcrft2.ccl", "w") as f:
+    # print(tagged_text)
+    with open("xd.ccl", "w") as f:
         f.write(tagged_text)
 
 
